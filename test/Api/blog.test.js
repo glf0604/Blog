0471863f527560e02f7f75e352e63d620c96da28
linfeng guan <928497141@qq.com>
2020-01-07 10:16:38 +0000

    Blog

--- test/Api/blog.test.js
+++ test/Api/blog.test.js
@@ -1,14 +1,14 @@
 // blog.test.js
-const request = require('superagent')
-const expect = require('chai').expect
+const request = require('superagent');
+const expect = require('chai').expect;
 
-let api_fix = 'http://localhost:8888/admin'
+let api_fix = 'http://localhost:8888/admin';
 
 describe('test GET /category', function() {
   // Setup Fake rendering
   beforeEach(() => {
     console.info('blogAPI test start')
-  })
+  });
 
   it.skip('should return 200 OK', async function(done) {
     request(api_fix)
@@ -16,29 +16,29 @@ describe('test GET /category', function() {
       .set('Content-Type', 'text/html')
       .expect(200, done)
   })
-})
+});
 
 describe('test GET /category/add', function(done) {
   // Setup Fake rendering
   beforeEach(() => {
     console.info('blog test start')
-  })
+  });
 
   it('should return 200 OK', async function(done) {
-    done()
+    done();
     let data = await request(api_fix).get('/category/add')
 
     // .set('Content-Type', 'text/html')
     // .expect(200, done)
     // done()
   })
-})
+});
 
 describe('test GET /category/add', function() {
   // Setup Fake rendering
   beforeEach(() => {
     console.info('blogAPI test start')
-  })
+  });
 
   it.skip('should return 200 OK', async function(done) {
     request(api_fix)
@@ -46,13 +46,13 @@ describe('test GET /category/add', function() {
       // .set('Content-Type', 'text/html')
       .expect(200, done)
   })
-})
+});
 
 describe('test GET /category/add', function() {
   // Setup Fake rendering
   beforeEach(() => {
     console.info('blogAPI test start')
-  })
+  });
 
   it.skip('should return 200 OK', async function(done) {
     request(api_fix)
@@ -60,18 +60,18 @@ describe('test GET /category/add', function() {
       .set('Content-Type', 'text/html')
       .expect(200, done)
   })
-})
+});
 
 describe('test GET /category/edit', function() {
   // Setup Fake rendering
   beforeEach(() => {
     console.info('blogAPI test start')
-  })
+  });
 
   it('should return 200 OK', async function(done) {
-    done()
+    done();
     request(api_fix).get('/category/edit')
     // .set('Content-Type', 'text/html')
     // .expect(200, done)
   })
-})
+});
