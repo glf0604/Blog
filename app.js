3c204029417825faac234712e24f2d965a8a18f7
linfeng guan <928497141@qq.com>
2020-01-07 12:02:44 +0000

    Blog

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var express = require('express')
-var swig = require('swig')
-var mongoose = require('mongoose')
-var bodyParser = require('body-parser')
-var Cookies = require('cookies')
+var express = require('express');
+var swig = require('swig');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Cookies = require('cookies');
 
-var expressWs = require('express-ws')
-var app = express()
+var expressWs = require('express-ws');
+var app = express();
 
-var User = require('./models/User')
+var User = require('./models/User');
 
 var WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({
     port: 3000 //监听接口
     // verifyClient: socketVerify //可选，验证连接函数
-  })
+  });
 
 //广播
 wss.broadcast = function broadcast(s, ws) {
@@ -21,16 +21,16 @@ wss.broadcast = function broadcast(s, ws) {
   // debugger;
   wss.clients.forEach(function each(client) {
     // if (typeof client.user != "undefined") {
-    if (s == 1) {
+    if (s === 1) {
       // client.send(ws.user + ":" + ws.msg);
       client.send(ws.content)
     }
-    if (s == 0) {
+    if (s === 0) {
       client.send(ws + 'Exit')
     }
     // }
   })
-}
+};
 
 // 初始化
 wss.on('connection', function(ws) {
@@ -40,16 +40,16 @@ wss.on('connection', function(ws) {
   // ws.send('你是第' + wss.clients.length + '位');
   // 发送消息
   ws.on('message', function(jsonStr, flags) {
-    console.log(jsonStr)
-    var obj = eval('(' + jsonStr + ')')
-    this.user = obj
+    console.log(jsonStr);
+    var obj = eval('(' + jsonStr + ')');
+    this.user = obj;
     if (typeof this.user.msg != 'undefined') {
-      if (this.user.msg == 'login') {
+      if (this.user.msg === 'login') {
         wss.broadcast(1, obj)
-      } else if (this.user.msg == 'comment') {
+      } else if (this.user.msg === 'comment') {
       }
     }
-  })
+  });
   // 退出聊天
   ws.on('close', function(close) {
     try {
@@ -58,7 +58,7 @@ wss.on('connection', function(ws) {
       console.log('Refresh')
     }
   })
-})
+});
 // expressWs(app);
 
 // app.ws('/socketTest', function (ws, req){
@@ -75,23 +75,23 @@ wss.on('connection', function(ws) {
 //     })
 // })
 
-app.use('/public', express.static(__dirname + '/public'))
+app.use('/public', express.static(__dirname + '/public'));
 
-app.engine('html', swig.renderFile)
-app.set('views', './views')
-app.set('view engine', 'html')
-swig.setDefaults({ cache: false })
+app.engine('html', swig.renderFile);
+app.set('views', './views');
+app.set('view engine', 'html');
+swig.setDefaults({ cache: false });
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-  req.cookies = new Cookies(req, res)
-  req.userInfo = {}
+  req.cookies = new Cookies(req, res);
+  req.userInfo = {};
   if (req.cookies.get('userInfo')) {
     try {
-      req.userInfo = JSON.parse(req.cookies.get('userInfo'))
+      req.userInfo = JSON.parse(req.cookies.get('userInfo'));
       User.findById(req.userInfo._id).then(function(userInfo) {
-        req.userInfo.isAdmin = Boolean(userInfo.isAdmin)
+        req.userInfo.isAdmin = Boolean(userInfo.isAdmin);
         next()
       })
     } catch (e) {
@@ -100,11 +100,11 @@ app.use(function(req, res, next) {
   } else {
     next()
   }
-})
+});
 
-app.use('/admin', require('./routers/admin'))
-app.use('/api', require('./routers/api'))
-app.use('/', require('./routers/main'))
+app.use('/admin', require('./routers/admin'));
+app.use('/api', require('./routers/api'));
+app.use('/', require('./routers/main'));
 
 /*var io = require('socket.io');
 // var io = require('socket.io')(http);
@@ -124,8 +124,8 @@ mongoose.connect('mongodb://localhost:27019/blog', { useNewUrlParser: true, useU
   if (err) {
     console.log('Fail')
   } else {
-    app.listen(8888)
+    app.listen(8888);
     console.info('mongodb connect successful')
     console.log('Listen at http//:localhost:8888')
   }
-})
+});
