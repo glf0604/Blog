eb76ae926beeae1a46c17b08f29c2d6d32a55ac9
linfeng guan <928497141@qq.com>
2020-01-07 20:16:25 +0000

    Blog

--- routers/main.js
+++ routers/main.js
@@ -1,30 +1,30 @@
-var express = require('express')
-var router = express.Router()
-var Category = require('../models/Category')
-var Content = require('../models/Content')
+var express = require('express');
+var router = express.Router();
+var Category = require('../models/Category');
+var Content = require('../models/Content');
 
-var data
+var data;
 
 router.use(function(req, res, next) {
   data = {
     userInfo: req.userInfo,
     categories: []
-  }
+  };
 
   Category.find().then(function(categories) {
-    data.categories = categories
+    data.categories = categories;
     next()
   })
-})
+});
 
 router.get('/', function(req, res, next) {
-  data.category = req.query.category || ''
-  data.count = 0
-  data.page = Number(req.query.page || 1)
-  data.limit = 10
-  data.pages = 0
+  data.category = req.query.category || '';
+  data.count = 0;
+  data.page = Number(req.query.page || 1);
+  data.limit = 10;
+  data.pages = 0;
 
-  var where = {}
+  var where = {};
   if (data.category) {
     where.category = data.category
   }
@@ -33,11 +33,11 @@ router.get('/', function(req, res, next) {
     .count()
     .then(function(count) {
       data.count = count
-      data.pages = Math.ceil(data.count / data.limit)
-      data.page = Math.min(data.page, data.pages)
-      data.page = Math.max(data.page, 1)
+      data.pages = Math.ceil(data.count / data.limit);
+      data.page = Math.min(data.page, data.pages);
+      data.page = Math.max(data.page, 1);
 
-      var skip = (data.page - 1) * data.limit
+      var skip = (data.page - 1) * data.limit;
 
       return Content.where(where)
         .find()
@@ -49,24 +49,24 @@ router.get('/', function(req, res, next) {
         })
     })
     .then(function(contents) {
-      data.contents = contents
+      data.contents = contents;
       res.render('main/index', data)
     })
-})
+});
 
 router.get('/view', function(req, res) {
-  var contentId = req.query.contentid || ''
+  var contentId = req.query.contentid || '';
 
   Content.findOne({
     _id: contentId
   }).then(function(content) {
-    data.content = content
+    data.content = content;
 
-    content.views++
-    content.save()
+    content.views++;
+    content.save();
 
     res.render('main/view', data)
   })
-})
+});
 
-module.exports = router
+module.exports = router;
