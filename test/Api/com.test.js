5ccecc5a29ebd9ab532e2e1b6e0948c7d0f9ded4
linfeng guan <928497141@qq.com>
2020-01-07 10:17:18 +0000

    Blog

--- test/Api/com.test.js
+++ test/Api/com.test.js
@@ -1,21 +1,21 @@
 // com.test.js
-const request = require('superagent')
-const expect = require('chai').expect
+const request = require('superagent');
+const expect = require('chai').expect;
 
-let api_fix = 'http://localhost:8888/api'
+let api_fix = 'http://localhost:8888/api';
 
 describe('test GET /category', function() {
   // Setup Fake rendering
   beforeEach(() => {
     console.info('public api test start')
-  })
+  });
 
   it.skip('should return 200 OK', async function(done) {
     request(api_fix)
       .get('/comment')
       .set('Accept', 'application/json')
       .expect(200, done)
-  })
+  });
 
   it.skip('should return 200 OK', async function(done) {
     request(api_fix)
@@ -23,4 +23,4 @@ describe('test GET /category', function() {
       .set('Accept', 'application/json')
       .expect(200, done)
   })
-})
+});
